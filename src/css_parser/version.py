635250227857be8032cd6d738ec254c6e6849a2a
Kovid Goyal <kovid@kovidgoyal.net>
2020-10-24 12:41:12 +0530

    version 1.0.5

--- src/css_parser/version.py
+++ src/css_parser/version.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8
 # License: LGPLv3 Copyright: 2018, Kovid Goyal <kovid at kovidgoyal.net>
 
-version = (1, 0, 4)
+version = (1, 0, 5)
 VERSION = '.'.join(map(str, version))
 
 __version__ = '%s $Id$' % VERSION
