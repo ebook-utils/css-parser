8d551c561c25aa0e974c3014a8e88ed36fca6e59
Kovid Goyal <kovid@kovidgoyal.net>
2026-06-10 09:38:01 +0530

    Remove deprecated setuptools test command

--- setup.py
+++ setup.py
@@ -4,42 +4,13 @@
 
 import ast
 import re
-import sys
-import os
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test
 
 # extract the version without importing the module
 VERSION = open('src/css_parser/version.py', 'rb').read().decode('utf-8')
 VERSION = '.'.join(map(str, ast.literal_eval(re.search(r'^version\s+=\s+(.+)', VERSION, flags=re.M).group(1))))
 long_description = '\n' + open('README.md', 'rb').read().decode('utf-8') + '\n'  # + read('CHANGELOG.txt')
-
-
-class Test(test):
-
-    user_options = [
-        ('which-test=', 'w', "Specify which test to run as either"
-            " the test method name (without the leading test_)"
-            " or a module name with a trailing period"),
-    ]
-
-    def initialize_options(self):
-        self.which_test = None
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import importlib
-        orig = sys.path[:]
-        try:
-            sys.path.insert(0, os.getcwd())
-            m = importlib.import_module('run_tests')
-            which_test = (self.which_test,) if self.which_test else ()
-            m.run_tests(which_test)
-        finally:
-            sys.path = orig
 
 
 setup(
@@ -50,7 +21,6 @@ setup(
     description='A CSS Cascading Style Sheets library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    cmdclass={'test': Test},
     author='Various People',
     author_email='redacted@anonymous.net',
     url='https://github.com/ebook-utils/css-parser',
